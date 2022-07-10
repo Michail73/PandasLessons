28a5e8ebdb459f9e83f302fe54ce71e4c17480bb
michail73 <roc7007@yandex.ru>
2022-07-10 20:00:13 +0300

    Initial commit

---
+++ welcome.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+dict_city = {"City": ["Moscow", "Saint-Petersburg", "Novosibirsk"],
+             "Population" : [12678079, 5398064, 1625631]}
+
+
+df = pd.DataFrame(dict_city)
